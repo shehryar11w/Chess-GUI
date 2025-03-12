35d7c924fdc3e84d5c233eacdedfe8a6cdf3f607
fayzan101 <faizanbasheer38@gmail.com>
2025-03-12 14:47:06 +0500

    Update Piece.h

--- src/Piece.h
+++ src/Piece.h
@@ -2,8 +2,11 @@
 #define PIECE_H
 
 #include "raylib.h"
+#include "Forward.h"
 #include <vector>
-#include "Game.h" 
+
+// Forward declarations
+class Game;
 
 class Piece {
 protected:
@@ -13,15 +16,22 @@ protected:
 
 public:
     Piece(int xPos, int yPos, Texture2D tex, bool white) 
-        : x(xPos), y(yPos), texture(tex), isWhite(white) {}
-    virtual ~Piece() = default;
+        : x(xPos), y(yPos), isWhite(white) {
+        texture = tex;  // Texture is managed by Team class
+    }
+    
+    virtual ~Piece() {
+        // Do not unload texture here - it's managed by Team class
+        texture.id = 0;
+    }
 
+    // Pure virtual function for valid moves - implementation in derived classes
     virtual std::vector<Vector2> GetValidMoves(const Game& game) const = 0;
 
     // Getters
     int GetX() const { return x; }
     int GetY() const { return y; }
-    Texture2D GetTexture() const { return texture; }
+    const Texture2D& GetTexture() const { return texture; }
     bool IsWhite() const { return isWhite; }
 
     // Setters
@@ -34,8 +44,6 @@ protected:
     bool IsValidPosition(int checkX, int checkY) const {
         return checkX >= 0 && checkX < 8 && checkY >= 0 && checkY < 8;
     }
-
-    // Moved GetPieceAt to Game.h to avoid incomplete type issue
 };
 
 #endif
