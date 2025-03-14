f70da84a5d5c6778f5e3377b781a4950f42949e7
fayzan101 <faizanbasheer38@gmail.com>
2025-03-14 14:47:38 +0500

    Pawn.h

--- src/Pawn.h
+++ src/Pawn.h
@@ -7,7 +7,7 @@
 class Pawn : public Piece {
 public:
     Pawn(int xPos, int yPos, Texture2D tex, bool white) 
-        : Piece(xPos, yPos, tex, white) {}
+        : Piece(xPos, yPos, tex, white, PieceType::PAWN) {}
 
     ~Pawn() {
         // Let raylib handle texture unloading
@@ -39,8 +39,25 @@ public:
             }
         }
 
+        // En passant
+        if (y == (isWhite ? 3 : 4)) {  // Check if the pawn is on the correct rank for en passant
+            for (int dx : {-1, 1}) {
+                int newX = x + dx;
+                if (IsValidPosition(newX, y)) {
+                    const Piece* adjacentPawn = game.GetPieceAt(newX, y);
+                    if (adjacentPawn && adjacentPawn->GetType() == PieceType::PAWN && adjacentPawn->IsWhite() != isWhite) {
+                        // Check if the last move was a two-square pawn advance
+                        if (game.GetLastMove().piece == adjacentPawn && abs(game.GetLastMove().end.y - game.GetLastMove().start.y) == 2) {
+                            moves.push_back({(float)newX, (float)newY});
+                        }
+                    }
+                }
+            }
+        }
+
         return moves;
     }
 };
 
 #endif
+
